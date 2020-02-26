328d878f6111225d57705a2fddbab1e66cee65b6
habbey sesan <habbeyunik@gmail.com>
2020-02-26 19:26:17 +0100

    async storage usage

--- App.js
+++ App.js
@@ -3,13 +3,22 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/home';
 import BrowserScreen from './src/screens/browser';
+import { SplashScreen } from 'expo';
+import MainScreen from './src/screens/main';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+
+  React.useEffect(() => {
+    SplashScreen.preventAutoHide();
+    setTimeout(SplashScreen.hide, 3000)
+  }, []);
+  
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Browser" component={BrowserScreen} />
       </Stack.Navigator>
